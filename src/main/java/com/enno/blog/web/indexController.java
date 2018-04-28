f642f49d34e7afde972e25444af408e963b5f0ed
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-04-28 22:33:01 +1200

    log

--- src/main/java/com/enno/blog/web/indexController.java
+++ src/main/java/com/enno/blog/web/indexController.java
@@ -3,18 +3,20 @@ package com.enno.blog.web;
 import com.enno.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class indexController {
 
-    @GetMapping("/")
-    public String index(){
-        int i = 9/0;
-        String blog = null;
-
-        if (blog == null){
-            throw  new NotFoundException("博客不存在");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
+//        int i = 9/0;
+//        String blog = null;
+//
+//        if (blog == null){
+//            throw  new NotFoundException("博客不存在");
+//        }
+        System.out.println("------------index-----------");
         return "index";
     }
 }
