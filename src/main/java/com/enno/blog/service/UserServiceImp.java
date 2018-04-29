24d1d55641c8baa9ae7676bf5a5c6c4e9cdb6de3
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-04-29 23:25:51 +1200

    MD5 encryption

--- src/main/java/com/enno/blog/service/UserServiceImp.java
+++ src/main/java/com/enno/blog/service/UserServiceImp.java
@@ -2,6 +2,7 @@ package com.enno.blog.service;
 
 import com.enno.blog.dao.UserRepository;
 import com.enno.blog.po.User;
+import com.enno.blog.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class UserServiceImp implements UserService{
 
     @Override
     public User checkuser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username,password);
+        User user = userRepository.findByUsernameAndPassword(username,MD5Utils.code(password));
         return user;
     }
 }
