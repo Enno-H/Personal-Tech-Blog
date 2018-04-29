96f5eed2bf8162b1da7408c0726c3bddfb0ef9d1
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-04-29 23:59:46 +1200

    Login interceptor

---
+++ src/main/java/com/enno/blog/interceptor/LoginInterceptor.java
@@ -0,0 +1,21 @@
+package com.enno.blog.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        //未登陆
+        if(request.getSession().getAttribute("user")==null){
+            response.sendRedirect("/admin");
+            return false;
+        }
+        return true;
+    }
+}
