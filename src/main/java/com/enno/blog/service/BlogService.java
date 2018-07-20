29191ac766b475bb90726d5ae593503a488b7a2a
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-07-20 12:51:56 +1200

    Search blogs by tags

--- src/main/java/com/enno/blog/service/BlogService.java
+++ src/main/java/com/enno/blog/service/BlogService.java
@@ -17,6 +17,8 @@ public interface BlogService {
 
     Page<Blog> listBlog(Pageable pageable);
 
+    Page<Blog> listBlog(Long tagId, Pageable pageable);
+
     Page<Blog> listBlog(String query, Pageable pageable);
 
     List<Blog> listRecommendBlogTop(Integer size);
--- src/main/java/com/enno/blog/service/BlogServiceImp.java
+++ src/main/java/com/enno/blog/service/BlogServiceImp.java
@@ -17,10 +17,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -79,6 +76,17 @@ public class BlogServiceImp implements BlogService {
     @Override
     public Page<Blog> listBlog(Pageable pageable) {
         return blogRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Blog> listBlog(Long tagId, Pageable pageable) {
+        return blogRepository.findAll(new Specification<Blog>() {
+            @Override
+            public Predicate toPredicate(Root<Blog> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+                Join join = root.join("tags");
+                return cb.equal(join.get("id"),tagId);
+            }
+        },pageable);
     }
 
     @Override
