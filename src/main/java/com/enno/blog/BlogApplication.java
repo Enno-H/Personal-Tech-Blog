07f5a485e18857d66ced0ef37cc84e6bc67f7497
Xiaolong <xiaolong_huang@foxmail.com>
2019-03-13 11:44:12 +1300

    packaging war

--- src/main/java/com/enno/blog/BlogApplication.java
+++ src/main/java/com/enno/blog/BlogApplication.java
@@ -2,11 +2,12 @@ package com.enno.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 }
